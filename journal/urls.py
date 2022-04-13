74a53ed805a99eefa2a425dffea54778bc51e9aa
Adrianus West <adrianus_west@protonmail.com>
2022-04-14 01:32:25 +0300

    Add primary (C)RUD)-functional

--- journal/urls.py
+++ journal/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('journal/list', views.JournalListView.as_view(), name='journal_list'),
+    path('journal/create_journal', views.JournalCreateView.as_view(), name='create_journal'),
+    path('journal/update_journal/<pk>', views.JournalUpdateView.as_view(), name='update_journal'),
+    path('journal/delete_journal/<pk>', views.JournalDeleteView.as_view(), name='delete_journal'),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,12 +1,40 @@
 from django.shortcuts import render
 from .models import Journal
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
-# Создаем JournalListView, который будет получать все записи Journal
+# Создаём JournalListView, который будет получать все записи Journal
 # в базе данных и передавать информацию в шаблон для отображения
 
 class JournalListView(ListView):
     model = Journal
-    template_name = 'journal_list.html'
+    template_name = 'journal/journal_list.html'
     context_object_name = 'journal_list'
+
+
+# Создаём JournalCreateView, который будет добавлять новую запись
+
+class JournalCreateView(CreateView):
+    model = Journal
+    template_name = 'journal/create_journal_form.html'
+    fields = ['title', 'description']
+    success_url = reverse_lazy('journal_list')
+
+
+# Создаём JournalUpdateView, который будет обновлять записи
+
+class JournalUpdateView(UpdateView):
+    model = Journal
+    template_name = 'journal/update_journal_form.html'
+    fields = ['title', 'description']
+    success_url = reverse_lazy('journal_list')
+
+
+# Создаём JournalDeleteView, который будет удалять записи
+
+class JournalDeleteView(DeleteView):
+    model = Journal
+    template_name = 'journal/delete_journal_form.html'
+    fields = ['title', 'description']
+    success_url = reverse_lazy('journal_list')
